84e2c28a96002cfc81701ca01780a3e3fdd96424
Henry Dowling <htdowling@gmail.com>
2025-07-15 00:11:29 -0700

    added cors to backend

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://noether.to", "https://www.noether.to"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
